11c56eb9ef5332dc65af47898c3df8b7fec48530
yuchenyip <yuchenyip@gmail.com>
2025-03-21 03:19:36 -0700

    Update robots.ts to use metadata route format

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,17 @@
 import { publicUrl } from "@/env.mjs";
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+export const revalidate = false;
+
 export default function robots(): MetadataRoute.Robots {
 	return {
-		rules: {
-			userAgent: "*",
-			allow: "/",
-		},
-		sitemap: publicUrl + "/sitemap.xml",
+		rules: [
+			{
+				userAgent: "*",
+				allow: "/",
+			},
+		],
+		sitemap: `${publicUrl}/sitemap.xml`,
 	};
 }
